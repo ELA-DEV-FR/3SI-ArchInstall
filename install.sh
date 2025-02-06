81745ed4ae7511620b55ac46943ecf346f736713
ELA-DEV <96297263+ELA-DEV-FR@users.noreply.github.com>
2025-02-06 10:35:39 +0100

    Update install.sh - new disk partition and grub install

--- install.sh
+++ install.sh
@@ -31,68 +31,51 @@ pacman -S reflector -y
 reflector -c "US" -f 12 -l 10 -n 12 --save /etc/pacman.d/mirrorlist
 pacman -S cryptsetup -y 
 
-fdisk "/dev/sda"
-echo n 
-echo p 
-echo 1
-echo
-echo +512M # partition boot 
-echo n
-echo p 
-echo 2 
-echo 
-echo +10G # partition chiffrée 
-echo n
-echo p
-echo 3 
-echo 
-echo +10G # partition /home
-echo n 
-echo p 
-echo 4
-echo 
-echo +2G # partition /tmp 
-echo n 
-echo p 
-echo 5 
-echo  
-echo +10G # partition /var notamment pour virtualbox avec mountpoint specifique 
-echo n
-echo p 
-echo 6 
-echo 
-echo +5G # partition /share 
-echo n 
-echo p 
-echo 7 
-echo 
-echo +5G # pour le / 
+##############
+# BUILD DISK #
+##############
+bash disk.sh
+pacstrap /mnt base linux linux-firmware
+genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt
+ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
+hwclock --systohc
+echo "archlinux" > /etc/hostname
 
-echo w
-
-echo -n "azerty123" | cryptsetup luksFormat --type luks2 $ROOT_PARTITION
-
-
-
-mkfs.ext4 "/boot"
-mkfs.ext4 "/"
-mkfs.ext4 "/share"
-mkfs.ext4 "/tmp"
-mkfs.ext4 "/home"
-
-
-mount /dev/sda6 /mnt
+########
+# GRUB #
+########
+pacman -S grub os-prober
+grub-install --target=i386-pc /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
 
 ############
 # HYPRLAND #
 ############
 # DEPENDANCES :
-yay -S ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite libxrender pixman wayland-protocols cairo pango libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info cpio tomlplusplus hyprlang-git hyprcursor-git hyprwayland-scanner-git xcb-util-errors hyprutils-git glaze hyprgraphics-git
-pacman -S --noconfirm sudo hyprland
-pacman -S yay -y 
+# yay -S ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite libxrender pixman wayland-protocols cairo pango libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info cpio tomlplusplus hyprlang-git hyprcursor-git hyprwayland-scanner-git xcb-util-errors hyprutils-git glaze hyprgraphics-git
+# pacman -S --noconfirm sudo hyprland
+# pacman -S yay -y 
 
 #########
 # USERS #
 #########
-echo "père:azerty123" | chpasswd
-useradd -m "fiston:azerty123" | chpasswd 
+useradd -m -s /bin/bash papa
+echo "papa:azerty123" | chpasswd
+useradd -m -s /bin/bash fiston
+echo "fiston:azerty123" | chpasswd
+
+########
+# SUDO #
+########
+pacman -S sudo
+visudo
+echo "papa ALL=(ALL) ALL" | tee -a /etc/sudoers
+
+##########
+# ENDING #
+##########
+echo -e "${GREEN}Installation terminée avec succès ! Redémarrage en cours...${RESET}"
+exit
+umount -R /mnt
+reboot
