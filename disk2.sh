a88ad1e67e7ef217d8b4e96ccea345a3666a0fa9
SKIPLALESSIVE <julienmans@yahoo.fr>
2025-02-12 21:38:10 +0100

    tst

--- disk2.sh
+++ disk2.sh
@@ -13,9 +13,7 @@ echo -e "${CYAN}-----------------------------------------------${RESET}"
 # Variables
 DISK="/dev/sda"
 EFI_SIZE=512
-VM_SIZE=$((20 * 1024))
-SHARE_SIZE=$((5 * 1024))
-OPTIONAL_SIZE=$((10 * 1024))
+LVM_SIZE=$((50 * 1024))  # Taille de la partition LVM
 
 ###################
 # PARTITIONNEMENT #
@@ -24,10 +22,7 @@ echo -e "${CYAN} Création des partitions...${RESET}"
 parted -s $DISK mklabel gpt
 parted -s $DISK mkpart ESP fat32 1MiB ${EFI_SIZE}MiB
 parted -s $DISK set 1 esp on
-parted -s $DISK mkpart VM ext4 ${EFI_SIZE}MiB $((EFI_SIZE + VM_SIZE))MiB
-parted -s $DISK mkpart SHARE ext4 $((EFI_SIZE + VM_SIZE))MiB $((EFI_SIZE + VM_SIZE + SHARE_SIZE))MiB
-parted -s $DISK mkpart OPTIONAL ext4 $((EFI_SIZE + VM_SIZE + SHARE_SIZE))MiB $((EFI_SIZE + VM_SIZE + SHARE_SIZE + OPTIONAL_SIZE))MiB
-parted -s $DISK mkpart DATA ext4 $((EFI_SIZE + VM_SIZE + SHARE_SIZE + OPTIONAL_SIZE))MiB 100%
+parted -s $DISK mkpart LVM ext4 ${EFI_SIZE}MiB $((EFI_SIZE + LVM_SIZE))MiB
 sleep 2
 
 ##################
@@ -35,28 +30,43 @@ sleep 2
 ##################
 echo -e "${CYAN} Formatage des partitions...${RESET}"
 mkfs.fat -F32 ${DISK}1
-mkfs.ext4 ${DISK}2
-mkfs.ext4 ${DISK}4
-mkfs.ext4 ${DISK}5
+
+###############
+# CONFIG LVM #
+###############
+echo -e "${CYAN} Configuration de LVM...${RESET}"
+pvcreate ${DISK}2
+vgcreate vg0 ${DISK}2
+lvcreate -L 20G -n root vg0
+lvcreate -L 10G -n home vg0
+lvcreate -L 10G -n var vg0
 
 ###############
 # CHIFFREMENT #
 ###############
-echo -e "${CYAN} Mise en place du chiffrement LUKS pour SHARE...${RESET}"
-echo -n "azerty123" | cryptsetup luksFormat --type luks2 ${DISK}3
-echo -n "azerty123" | cryptsetup open ${DISK}3 share_crypt
-mkfs.ext4 /dev/mapper/share_crypt
+echo -e "${CYAN} Mise en place du chiffrement LUKS pour les volumes logiques...${RESET}"
+echo -n "azerty123" | cryptsetup luksFormat --type luks2 /dev/vg0/root
+echo -n "azerty123" | cryptsetup luksFormat --type luks2 /dev/vg0/home
+echo -n "azerty123" | cryptsetup luksFormat --type luks2 /dev/vg0/var
+
+echo -n "azerty123" | cryptsetup open /dev/vg0/root root_crypt
+echo -n "azerty123" | cryptsetup open /dev/vg0/home home_crypt
+echo -n "azerty123" | cryptsetup open /dev/vg0/var var_crypt
+
+mkfs.ext4 /dev/mapper/root_crypt
+mkfs.ext4 /dev/mapper/home_crypt
+mkfs.ext4 /dev/mapper/var_crypt
 
 ###########
 # MONTAGE #
 ###########
 echo -e "${CYAN} Montage des partitions...${RESET}"
-mount ${DISK}5 /mnt
+mount /dev/mapper/root_crypt /mnt
 mkdir -p /mnt/boot/efi
-mkdir -p /mnt/share
-mkdir -p /mnt/data
+mkdir -p /mnt/home
+mkdir -p /mnt/var
 mount ${DISK}1 /mnt/boot/efi
-mount /dev/mapper/share_crypt /mnt/share
-mount ${DISK}2 /mnt/vm
+mount /dev/mapper/home_crypt /mnt/home
+mount /dev/mapper/var_crypt /mnt/var
 
 echo -e "${GREEN} Partitionnement terminé !${RESET}"
