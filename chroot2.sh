13d54e60f9cc1cd4a5c7fe9ebc4d3ee5b033fbf1
SKIPLALESSIVE <julienmans@yahoo.fr>
2025-02-12 19:06:04 +0100

    wip

--- chroot2.sh
+++ chroot2.sh
@@ -7,7 +7,7 @@ ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
 hwclock --systohc
 echo "archlinux" > /etc/hostname
 
-pacman -S grub os-prober efibootmgr nano sudo networkmanager --noconfirm
+pacman -S grub os-prober efibootmgr nano sudo networkmanager gcc firefox-esr  virtualbox virtualbox-host-dkms --noconfirm
 mkdir -p /boot/efi
 mount ${DISK}1 /boot/efi
 
@@ -18,15 +18,16 @@ grub-mkconfig -o /boot/grub/grub.cfg
 
 useradd -m -s /bin/bash papa
 echo "papa:azerty123" | chpasswd
+
+echo "papa ALL=(ALL) ALL" >> /etc/sudoers
+
 useradd -m -s /bin/bash fiston
 echo "fiston:azerty123" | chpasswd
 groupadd share_users
 usermod -aG share_users papa
 usermod -aG share_users fiston
-systemctl enable Networkmanager
+
+systemctl enable NetworkManager
 loadkeys fr 
 chown root:share_users /mnt/share
 chmod 770 /mnt/share
-
-
-echo "papa ALL=(ALL) ALL" >> /etc/sudoers
