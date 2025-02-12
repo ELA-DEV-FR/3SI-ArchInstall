57ea8c895685a490a1c4874d10838d1138bc1c4d
SKIPLALESSIVE <julienmans@yahoo.fr>
2025-02-12 22:28:25 +0100

    wip

--- chroot2.sh
+++ chroot2.sh
@@ -7,7 +7,7 @@ ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
 hwclock --systohc
 echo "archlinux" > /etc/hostname
 
-pacman -S grub os-prober efibootmgr nano sudo networkmanager hyprland firefox virtualbox virtualbox-host-dkms --noconfirm
+pacman -S grub os-prober efibootmgr nano sudo networkmanager hyprland firefox virtualbox virtualbox-host-dkms cryptsetup lvm2 --noconfirm
 mkdir -p /boot/efi
 mount ${DISK}1 /boot/efi
 
@@ -27,6 +27,9 @@ systemctl daemon-reload
 echo "GRUB_ENABLE_CRYPTODISK=y" >> /etc/default/grub
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
+
+sed -i 's/^HOOKS=(.*)/HOOKS=(base udev autodetect keyboard keymap modconf block encrypt lvm2 filesystems fsck)/' /etc/mkinitcpio.conf
+mkinitcpio -P
 
 systemctl enable NetworkManager
 
