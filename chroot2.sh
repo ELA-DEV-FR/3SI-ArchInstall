2f147e2187b955a17782376d4efb34548e16d96f
SKIPLALESSIVE <julienmans@yahoo.fr>
2025-02-12 20:45:48 +0100

    wip

--- chroot2.sh
+++ chroot2.sh
@@ -13,6 +13,7 @@ mount ${DISK}1 /boot/efi
 mkdir -p /mnt/home/papa/VirtualBox\ VMs/
 echo "GRUB_ENABLE_CRYPTODISK=y" >> /etc/default/grub
 echo "UUID=$(blkid -s UUID -o value ${DISK}2) /home/papa/VirtualBox\ VMs/ ext4 defaults 0 2" >> /etc/fstab
+echo "share UUID=$(blkid -s UUID -o value ${DISK}3) none luks" >> /etc/crypttab
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
 
