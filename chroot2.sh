eaaa4666c215224b640cc1191c3483b31c265174
SKIPLALESSIVE <julienmans@yahoo.fr>
2025-02-13 21:52:00 +0100

    wip

--- chroot2.sh
+++ chroot2.sh
@@ -1,88 +1,69 @@
 #!/bin/bash
 
-# --- Couleurs (optionnel) ---
 RED="\e[31m"
 GREEN="\e[32m"
 CYAN="\e[36m"
 RESET="\e[0m"
 
-DISK="/dev/sda"  # Adapter si besoin
+DISK="/dev/sda" 
 echo -e "${CYAN}Configuration du système en chroot...${RESET}"
 
-# 1) Fuseau horaire & horloge
 ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
 hwclock --systohc
 echo "archlinux" > /etc/hostname
 
-# 2) Installation des paquets essentiels
 pacman -S --noconfirm grub os-prober efibootmgr nano sudo networkmanager \
-                      hyprland firefox virtualbox virtualbox-host-dkms \
-                      cryptsetup lvm2
+  hyprland firefox virtualbox virtualbox-host-dkms cryptsetup lvm2
+pacman -S --noconfirm i3 i3status i3lock dmenu feh xorg-xinit xorg-server \
+  xorg-xrandr xorg-xrdb ttf-dejavu compton
 
-# 3) Monter /boot/efi (pour réinstaller GRUB EFI si besoin)
+
 mkdir -p /boot/efi
 mount "${DISK}1" /boot/efi
 
-# 4) Récupération de l'UUID de la partition chiffrée
 CRYPT_UUID=$(blkid -s UUID -o value "${DISK}2")
-echo -e "${CYAN}UUID de la partition LUKS : ${CRYPT_UUID}${RESET}"
 
-# 5) Configuration /etc/crypttab
-cat <<EOF > /etc/crypttab
-lvm_crypt UUID=${CRYPT_UUID} none luks
+
+echo "lvm_crypt UUID=${CRYPT_UUID} none luks" > /etc/crypttab
+
+echo "HOOKS=(base udev autodetect keyboard keymap consolefont modconf block encrypt lvm2 filesystems fsck)" >> /etc/mkinitcpio.conf
+
+
+cat <<EOF > /etc/default/grub
+GRUB_ENABLE_CRYPTODISK=y
+GRUB_PRELOAD_MODULES="luks2 part_gpt cryptodisk gcry_rijndael gcry_sha512 lvm ext2"
+GRUB_CMDLINE_LINUX="cryptdevice=UUID=${CRYPT_UUID}:lvm_crypt root=/dev/mapper/vg0-root"
 EOF
 
-# 6) Configuration de mkinitcpio
-# On remplace toute la ligne HOOKS=... par notre suite de hooks
-# (ordre important : encrypt puis lvm2)
-sed -i 's|^HOOKS=.*|HOOKS=(base udev autodetect keyboard keymap consolefont modconf block encrypt lvm2 filesystems fsck)|' /etc/mkinitcpio.conf
-
-# 7) Configuration de /etc/default/grub
-#    - GRUB_ENABLE_CRYPTODISK=y pour demander à GRUB de gérer LUKS2
-#    - cryptdevice=UUID=xxx:lvm_crypt root=... pointe vers /dev/mapper/vg0-root
-{
-  echo "GRUB_ENABLE_CRYPTODISK=y"
-  echo "GRUB_PRELOAD_MODULES=\"luks2 part_gpt cryptodisk gcry_rijndael gcry_sha512 lvm ext2\""
-  echo "GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=${CRYPT_UUID}:lvm_crypt root=/dev/mapper/vg0-root\""
-} > /etc/default/grub
-
-# (Facultatif) Si on veut être sûr de forcer la ligne CMDLINE_LINUX :
-sed -i "s|^GRUB_CMDLINE_LINUX=.*|GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=${CRYPT_UUID}:lvm_crypt root=/dev/mapper/vg0-root\"|" /etc/default/grub
-
-# 8) Activation du VG au cas où
 vgchange -ay
 
-# 9) Regénération de l’initramfs
+
 mkinitcpio -P
 
-# 10) Installation du chargeur GRUB en mode EFI
-grub-install --target=x86_64-efi \
-             --efi-directory=/boot/efi \
-             --modules="luks2 part_gpt cryptodisk gcry_rijndael gcry_sha512 lvm ext2" \
-             --recheck
 
-# 11) Génération de grub.cfg
+grub-install --target=x86_64-efi --efi-directory=/boot/efi  --modules="luks2 part_gpt cryptodisk gcry_rijndael gcry_sha512 lvm ext2" --recheck
+
+
 grub-mkconfig -o /boot/grub/grub.cfg
 
-# 12) Réactivation initramfs (pas forcément indispensable, mais on peut le faire)
+
 mkinitcpio -P
 
-# 13) Activation NetworkManager
+
 systemctl enable NetworkManager
 
-# 14) Création utilisateurs (exemple)
+
 useradd -m -s /bin/bash papa
 echo "papa:azerty123" | chpasswd
 
 useradd -m -s /bin/bash fiston
 echo "fiston:azerty123" | chpasswd
 
-# Droits VirtualBox
 mkdir -p "/home/papa/VirtualBox VMs/"
 chown -R papa:papa "/home/papa/VirtualBox VMs/"
 chmod 777 "/home/papa/VirtualBox VMs/"
 
-# 15) Modules à charger au démarrage (facultatif)
+
 cat <<EOF > /etc/modules-load.d/virtualbox.conf
 vboxdrv
 vboxnetflt
@@ -90,10 +71,6 @@ vboxnetadp
 dm-crypt
 EOF
 
-# 16) Sudo pour l’utilisateur "papa"
 echo "papa ALL=(ALL) ALL" >> /etc/sudoers
-
-# (Optionnel) Éviter de fermer le volume chiffré depuis le chroot : ça provoque souvent une erreur
-# cryptsetup close lvm_crypt
 
 echo -e "${GREEN}Configuration chroot terminée avec succès !${RESET}"
--- disk2.sh
+++ disk2.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# --- Couleurs (optionnel) ---
 RED="\e[31m"
 GREEN="\e[32m"
 CYAN="\e[36m"
@@ -10,30 +9,23 @@ echo -e "${CYAN}-----------------------------------------------${RESET}"
 echo -e "${GREEN}   Auto-Partitionnement & Chiffrement Arch    ${RESET}"
 echo -e "${CYAN}-----------------------------------------------${RESET}"
 
-DISK="/dev/sda"         # Adapter si besoin
-EFI_SIZE=512            # Taille de la partition EFI (en Mio)
-LVM_SIZE=$((60 * 1024)) # Taille de la partition LVM (60 Gio)
-PASSWD="azerty123"      # Mot de passe LUKS (exemple peu sécurisé !)
-
-echo -e "${CYAN}1) Création du label GPT...${RESET}"
+DISK="/dev/sda"         
+EFI_SIZE=512           
+LVM_SIZE=$((60 * 1024)) 
+PASSWD="azerty123"     
 parted -s "$DISK" mklabel gpt
-
-echo -e "${CYAN}2) Création de la partition EFI...${RESET}"
 parted -s "$DISK" mkpart ESP fat32 1MiB "${EFI_SIZE}MiB"
 parted -s "$DISK" set 1 esp on
 
-echo -e "${CYAN}3) Création de la partition LVM (chiffrée)...${RESET}"
+
 parted -s "$DISK" mkpart LVM ext4 "${EFI_SIZE}MiB" "$((EFI_SIZE + LVM_SIZE))MiB"
 
-echo -e "${CYAN}4) Formatage de la partition EFI en FAT32...${RESET}"
+
 mkfs.fat -F32 "${DISK}1"
 
-echo -e "${CYAN}5) Configuration LUKS sur ${DISK}2...${RESET}"
-# Pour de meilleures pratiques : --type luks2 --cipher aes-xts-plain64 --key-size 512 --hash sha512 --iter-time 2000 --pbkdf argon2id
-# Ici on reste proche de ton script existant :
+
 echo -n "$PASSWD" | cryptsetup luksFormat --type luks2 --pbkdf pbkdf2 --batch-mode "${DISK}2"
 
-echo -e "${CYAN}6) Ouverture du conteneur chiffré (lvm_crypt)...${RESET}"
 echo -n "$PASSWD" | cryptsetup open "${DISK}2" lvm_crypt
 partprobe "$DISK"
 
@@ -41,21 +33,21 @@ echo -e "${CYAN}7) Configuration LVM dans le conteneur chiffré...${RESET}"
 pvcreate /dev/mapper/lvm_crypt
 vgcreate vg0 /dev/mapper/lvm_crypt
 
-# Exemple de volumes logiques
+
 lvcreate -L 20G -n root vg0
 lvcreate -L 10G -n home vg0
 lvcreate -L 10G -n var  vg0
 lvcreate -L 10G -n vm   vg0
 lvcreate -L  5G -n share vg0
 
-echo -e "${CYAN}8) Formatage des volumes logiques...${RESET}"
+
 mkfs.ext4 /dev/vg0/root
 mkfs.ext4 /dev/vg0/home
 mkfs.ext4 /dev/vg0/var
 mkfs.ext4 /dev/vg0/vm
 mkfs.ext4 /dev/vg0/share
 
-echo -e "${CYAN}9) Montage des partitions...${RESET}"
+
 mount /dev/vg0/root /mnt
 
 mkdir -p /mnt/boot/efi
